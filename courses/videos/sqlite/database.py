7a89bde090b58234faa2cb7d61a30e2df99a9a4c
Edoardo Nervo <edoardonervo96@gmail.com>
2023-11-19 20:01:22 +0100

    sqllite missing files

---
+++ courses/videos/sqlite/database.py
@@ -0,0 +1,96 @@
+import sqlite3
+
+def main():
+    # Create a connectiomn
+    # conn = sqlite3.connect(':memory:') # NOT PERMANENT
+    conn = sqlite3.connect('customer.db')  # PERMANENT 
+
+    # Create a c
+    c = conn.cursor() # Single cursor
+
+    many_customers = [
+        ('Wes', 'Brown', 'wes@gmail.com'),
+        ('Steph', 'Kuwe', 'steph@gmail.com'),
+        ('Edo', 'Ner', 'edo@gmail.com')
+        ] # data can be inserted with python tuples
+
+    # CREATE a table
+    # ------------------------------
+    # c.execute("""
+    #     CREATE TABLE customers (
+    #           first_name text, 
+    #           last_name text,
+    #           email text
+    #     )
+    # """)
+    # -------------------------------
+
+    # INSERT data in the table
+    # -------------------------------
+    # c.execute('INSERT INTO customers VALUES ("John", "Elder", "john@gmail.com")')
+    # c.execute('INSERT INTO customers VALUES ("Tim", "Smith", "tim@gmail.com")')
+    # c.execute('INSERT INTO customers VALUES ("Mary", "Brown", "mary@gmail.com")')
+    # -------------------------------
+
+    # -------------------------------
+    # c.executemany('INSERT INTO customers VALUES (?, ?, ?)', many_customers)
+    # -------------------------------
+
+    # FORMATTIN EXAMPLES
+    # ------------------------------- 
+    # c.execute('SELECT * FROM customers')
+    # print(c.fetchone())
+    # print(c.fetchmany(3))
+    # print(c.fetchall())
+    # items = c.fetchall()
+
+    # print("NAME" + '\t' + 'SURNAME' + '\t' + 'EMAIL')
+    # print('----------------------------------')
+    # for item in items:
+    #     print(item[0] + '\t ' + item[1] + '\t' + item[2])
+    # -----------------------------------
+
+    # PRIMAREY KEYS
+    # -----------------------------------
+    # c.execute('SELECT rowid, * FROM customers')
+
+    # items = c.fetchall()
+
+    # for item in items:
+    #     print(item)
+    # -----------------------------------
+
+    # WHERE CLAUSE
+    # -----------------------------------
+    # c.execute('SELECT * FROM customers WHERE last_name LIKE "Br%"')
+
+    # items = c.fetchall()
+
+    # for item in items:
+        # item
+    # -----------------------------------
+
+    # UPDATE RECORDS
+    # -----------------------------------
+    c.execute("""
+        UPDATE customers SET first_name = "BoB"
+        WHERE last_name = "Elder"
+""")
+
+    c.execute('SELECT rowid, * FROM customers')
+
+    items = c.fetchall()
+
+    for item in items:
+        print(item)
+    # -----------------------------------
+
+    # The commit command actually execute the commands
+    conn.commit()
+
+    # Close the connection
+    conn.close()    
+
+
+if __name__ == '__main__':
+    main()
